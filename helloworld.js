0a2ae5db72215a8d59b995bde86f21e6eac3fece
SumanthReddy06 <107369077+SumanthReddy06@users.noreply.github.com>
2023-10-09 22:12:52 -0400

    Update helloworld.js

--- helloworld.js
+++ helloworld.js
@@ -2,10 +2,10 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/sumanth', (req, res) => {
-  res.send("This is sumanth's World!")
+app.get('/bajjuri', (req, res) => {
+  res.send("This is bajjuri's World!")
 })
 
 app.listen(port, () => {
-  console.log(`praneeth app listening on port ${port}`)
+  console.log(`sumanth app listening on port ${port}`)
 })
