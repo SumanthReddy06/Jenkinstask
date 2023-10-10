2fe65203455686a9702019a1a50f8eef8abb29d2
SumanthReddy06 <107369077+SumanthReddy06@users.noreply.github.com>
2023-10-09 22:56:43 -0400

    Update and rename helloworld.js to sumanth.js

--- sumanth.js
+++ sumanth.js
@@ -7,5 +7,5 @@ app.get('/sumanth', (req, res) => {
 })
 
 app.listen(port, () => {
-  console.log(`praneeth app listening on port ${port}`)
+  console.log(`sumanth app listening on port ${port}`)
 })
